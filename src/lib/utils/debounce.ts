d80cece538a4c4e573e99d719ef6caa79693ccbb
Edan Harr <edan@analyticintelligencesolutions.com>
2025-11-22 01:58:09 -0800

    Delete src/lib/utils directory

--- src/lib/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-export function debounce<F extends (...args: any[]) => void>(fn: F, delay: number): F {
-  let timer: number | undefined;
-
-  return function debounced(this: any, ...args: any[]) {
-    window.clearTimeout(timer);
-    timer = window.setTimeout(() => fn.apply(this, args), delay);
-  } as F;
-}
--- src/lib/utils/math.ts
+++
@@ -1,4 +0,0 @@
-export function percent(part: number, total: number): number {
-  if (total === 0) return 0;
-  return Math.round((part / total) * 100);
-}
