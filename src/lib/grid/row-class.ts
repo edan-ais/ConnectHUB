24d24f87d98838600eb84cea54f442606493cc36
Edan Harr <edan@analyticintelligencesolutions.com>
2025-11-22 01:57:59 -0800

    Delete src/lib/grid directory

--- src/lib/grid/row-class.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from '../../types/product';
-import { rowBgClass } from './colors';
-
-export function rowClassName(product: Product): string {
-  return `${rowBgClass(product.validationStatus)} hover:bg-slate-200`;
-}
