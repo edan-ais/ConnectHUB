9abdd1391b8113728d6d4572dd1deb79b2ec60a6
Edan Harr <edan@analyticintelligencesolutions.com>
2025-11-22 02:05:30 -0800

    Delete src/hooks directory

--- src/hooks/useLastSaved.ts
+++
@@ -1,7 +0,0 @@
-import { useRecoilValue } from 'recoil';
-import { lastSavedAtState } from '../state/productAtom';
-
-export function useLastSaved() {
-  const lastSavedAt = useRecoilValue(lastSavedAtState);
-  return { lastSavedAt };
-}
