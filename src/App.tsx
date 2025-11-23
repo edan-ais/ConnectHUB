d5abc979fdefdee71e561e204e74d198bfd998df
Edan Harr <edan@analyticintelligencesolutions.com>
2025-11-22 16:20:39 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Shell } from "./components/layout/Shell";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const App: React.FC = () => {
   return <Shell />;
